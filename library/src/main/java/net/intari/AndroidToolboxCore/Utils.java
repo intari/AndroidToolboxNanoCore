b650a93eee8d24f1ef32445376395c5b6fe8a7ff
Dmitriy Kazimirov <dmitriy.kazimirov@viorsan.com>
2018-01-20 13:49:29 +0600

    make getUniquePsuedoID part of Core

--- library/src/main/java/net/intari/AndroidToolboxCore/Utils.java
+++ library/src/main/java/net/intari/AndroidToolboxCore/Utils.java
@@ -34,6 +34,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 
 /**
@@ -574,5 +575,45 @@ public class Utils {
         //потому что будет еще фактор масштабирования использоваться
         Geometry.Point result=new Geometry.Point((float)resultX,(float)resultY,0f);
         return result;
+    }
+
+    /**
+     * Based off http://stackoverflow.com/questions/2785485/is-there-a-unique-android-device-id
+     * not use play services ad id
+     * Return pseudo unique ID
+     * Will never return null. Could get collisions on API<9
+     * @return pseudo unique ID for this device
+     */
+    public static String getUniquePsuedoID() {
+        // If all else fails, if the user does have lower than API 9 (lower
+        // than Gingerbread), has reset their device or 'Secure.ANDROID_ID'
+        // returns 'null', then simply the ID returned will be solely based
+        // off their Android device information. This is where the collisions
+        // can happen.
+        // Thanks http://www.pocketmagic.net/?p=1662!
+        // Try not to use DISPLAY, HOST or ID - these items could change.
+        // If there are collisions, there will be overlapping data
+        String m_szDevIDShort = "35" + (Build.BOARD.length() % 10) + (Build.BRAND.length() % 10) + (Build.CPU_ABI.length() % 10) + (Build.DEVICE.length() % 10) + (Build.MANUFACTURER.length() % 10) + (Build.MODEL.length() % 10) + (Build.PRODUCT.length() % 10);
+
+        // Thanks to @Roman SL!
+        // http://stackoverflow.com/a/4789483/950427
+        // Only devices with API >= 9 have android.os.Build.SERIAL
+        // http://developer.android.com/reference/android/os/Build.html#SERIAL
+        // If a user upgrades software or roots their device, there will be a duplicate entry
+        String serial = null;
+        try {
+            serial = android.os.Build.class.getField("SERIAL").get(null).toString();
+
+            // Go ahead and return the serial for api => 9
+            return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
+        } catch (Exception exception) {
+            // String needs to be initialized
+            serial = "serial"; // some value
+        }
+
+        // Thanks @Joe!
+        // http://stackoverflow.com/a/2853253/950427
+        // Finally, combine the values we have found by using the UUID class to create a unique identifier
+        return new UUID(m_szDevIDShort.hashCode(), serial.hashCode()).toString();
     }
 }
