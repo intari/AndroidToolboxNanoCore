79ac421510f37a85cd7a7badbb7370d59f55c4d7
Dmitriy Kazimirov <dmitriy.kazimirov@viorsan.com>
2018-01-28 23:06:13 +0600

    Docs for logger helper

--- library/src/main/java/net/intari/AndroidToolboxCore/Extensions/Logger.kt
+++ library/src/main/java/net/intari/AndroidToolboxCore/Extensions/Logger.kt
@@ -14,61 +14,147 @@ import net.intari.CustomLogger.CustomLog
 */
 open class Logger(
         open val tag: String) {
-    inline operator fun invoke(message: () -> String) {
-        CustomLog.i(tag,message())
-    }
 
+
+    /**
+     * Logs exception
+     * @param cause
+     */
     inline operator fun invoke(cause: Throwable) {
         CustomLog.logException(tag,cause)
     }
+    /**
+     * Logs exception
+     * @param cause
+     */
     inline operator fun invoke(cause: Exception) {
         CustomLog.logException(tag,cause)
     }
+
+    /**
+     * Logs exception
+     * @param cause
+     */
     inline fun exception(t: Throwable) {
         CustomLog.logException(tag,t)
     }
+
+    /**
+     * Logs exception
+     * @param cause
+     */
     inline fun exception(t: Exception) {
         CustomLog.logException(tag,t)
     }
 
+    /**
+     * Logs message at info level
+     * @param message labmda to be evaluated for message text
+     */
+    inline operator fun invoke(message: () -> String) {
+        CustomLog.i(tag,message())
+    }
+
+    /**
+     * Logs message at info level
+     * @param message labmda to be evaluated for message text
+     */
     inline fun info(message: () -> String) {
         CustomLog.i(tag,message())
     }
+
+    /**
+     * Logs message at info level
+     * @param message labmda to be evaluated for message text
+     */
     inline fun i(message: () -> String) {
         CustomLog.i(tag,message())
     }
 
+    /**
+     * Logs message at log level
+     * @param message labmda to be evaluated for message text
+     */
     inline fun l(message: () -> String) {
         CustomLog.l(tag,message())
     }
+
+    /**
+     * Logs message at log level
+     * @param message labmda to be evaluated for message text
+     */
     inline fun log(message: () -> String) {
         CustomLog.l(tag,message())
     }
+
+    /**
+     * Logs message at debug level
+     * @param message labmda to be evaluated for message text
+     */
     inline fun debug(message: () -> String) {
         CustomLog.d(tag,message())
     }
+
+    /**
+     * Logs message at debug level
+     * @param message labmda to be evaluated for message text
+     */
     inline fun d(message: () -> String) {
         CustomLog.d(tag,message())
     }
+
+    /**
+     * Logs message at verbose level
+     * @param message labmda to be evaluated for message text
+     */
     inline fun v(message: () -> String) {
         CustomLog.v(tag,message())
     }
+
+    /**
+     * Logs message at verbose level
+     * @param message labmda to be evaluated for message text
+     */
     inline fun verbose(message: () -> String) {
         CustomLog.v(tag,message())
     }
+
+    /**
+     * Logs message at warning level
+     * @param message labmda to be evaluated for message text
+     */
     inline fun w(message: () -> String) {
         CustomLog.w(tag,message())
     }
+
+    /**
+     * Logs message at warning level
+     * @param message labmda to be evaluated for message text
+     */
     inline fun warning(message: () -> String) {
         CustomLog.w(tag,message())
     }
 
+    /**
+     * Logs message at error level
+     * @param message labmda to be evaluated for message text
+     */
     inline fun e(message: () -> String) {
         CustomLog.e(tag,message())
     }
+
+    /**
+     * Logs message at error level
+     * @param message labmda to be evaluated for message text
+     */
     inline fun error(message: () -> String) {
         CustomLog.e(tag,message())
     }
+
+    /**
+     * Put mark in log stream (see NSLogger's docs)
+     * @param message labmda to be evaluated for mark text
+     */
     inline fun mark(message: () -> String) {
         CustomLog.logMark(message())
     }
@@ -77,6 +163,14 @@ open class Logger(
 /**
  * Allows to create a [Logger] instance for a specific class. Note that loggers are not cached - each time this method
  * is called, a new instance of [Logger] is created, so it is advised to use loggers in companion objects.
+ * tag will be T's 'simple' name 
  * @return a new instance of [Logger] which contains the passed class name in log tags.
  */
-inline fun <reified T : Any> logger(): Logger = Logger(T::class.java.name)
+inline fun <reified T : Any> logger(): Logger = Logger(T::class.java.simpleName)
+/**
+ * Allows to create a [Logger] instance for a specific class. Note that loggers are not cached - each time this method
+ * is called, a new instance of [Logger] is created, so it is advised to use loggers in companion objects.
+ * tag will be T's full class name
+ * @return a new instance of [Logger] which contains the passed class name in log tags.
+ */
+inline fun <reified T : Any> loggerWithFullClassName(): Logger = Logger(T::class.java.name)
