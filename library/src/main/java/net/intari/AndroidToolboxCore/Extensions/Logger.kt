6c0dbf07f39294fc9421a976a28c87b621102369
Dmitriy Kazimirov <dmitriy.kazimirov@viorsan.com>
2018-01-29 09:22:09 +0600

    More complete wrapping of CustomLogger's functionality

--- library/src/main/java/net/intari/AndroidToolboxCore/Extensions/Logger.kt
+++ library/src/main/java/net/intari/AndroidToolboxCore/Extensions/Logger.kt
@@ -1,5 +1,6 @@
 package net.intari.AndroidToolboxCore.Extensions
 
+import android.content.Context
 import net.intari.CustomLogger.CustomLog
 
 /**
@@ -10,154 +11,165 @@ import net.intari.CustomLogger.CustomLog
 
 /**
 * A simple logging utility class which caches its tags.
-* @param tag name of the logger included in log tags. Proceeded with a prefix of the specified log type.
+* @param tag name of the logger included in log tags. 
 */
 open class Logger(
         open val tag: String) {
 
 
     /**
-     * Logs exception
-     * @param cause
+     * Sets user email
+     * @param userEmail labmda to be evaluated for user email
      */
-    inline operator fun invoke(cause: Throwable) {
-        CustomLog.logException(tag,cause)
-    }
+    inline fun setUserEmail(userEmail: () -> String) = CustomLog.setUserEmail(userEmail())
+
     /**
-     * Logs exception
-     * @param cause
+     * Sets user name
+     * @param labmda to be evaluated for user name
      */
-    inline operator fun invoke(cause: Exception) {
-        CustomLog.logException(tag,cause)
-    }
+    inline fun setUserName(userName: () -> String) = CustomLog.setUserName(userName())
+
+    /**
+     * Sets user identifier
+     * @param userIdentifier labmda to be evaluated for userIdentifier text
+     */
+    inline fun setUserIdentifier(userIdentifier: () -> String) = CustomLog.setUserIdentifier(userIdentifier())
+
+    /**
+     * Where NSLogger.app is running
+     * @param newLogHost hostname like host.domain.com
+     * @param newLogPort port number
+     * @return false if this function was arleady called
+     */
+    fun setLogDestination(newLogHost: String, newLogPort: Int): Boolean = CustomLog.setLogDestination(newLogHost,newLogPort)
+
+    /**
+     * Are should we use extra debugging features (like dump to logcat)
+     * @param newIsDebug labmda to be evaluated for isDebug
+     */
+    fun setIsDebug(newIsDebug: () -> Boolean) = CustomLog.setIsDebug(newIsDebug())
+
+    /**
+     * Is it ok to use Crashlytics? (not currently used)
+     * @param newIsLogCrashlytics
+     */
+    fun setLogCrashlytics(newIsLogCrashlytics: () -> Boolean) = CustomLog.setIsDebug(newIsLogCrashlytics())
+
+    /**
+     * Sets app context to use
+     * @param context labmda to be evaluated for context
+     */
+    fun setContext(context: () -> Context) = CustomLog.setContext(context())
 
     /**
      * Logs exception
      * @param cause
      */
-    inline fun exception(t: Throwable) {
-        CustomLog.logException(tag,t)
-    }
+    operator fun invoke(cause: Throwable) = CustomLog.logException(tag,cause)
 
     /**
      * Logs exception
      * @param cause
      */
-    inline fun exception(t: Exception) {
-        CustomLog.logException(tag,t)
-    }
+    operator fun invoke(cause: Exception) = CustomLog.logException(tag,cause)
+
+    /**
+     * Logs exception
+     * @param cause
+     */
+    fun exception(t: Throwable) = CustomLog.logException(tag,t)
+
+    /**
+     * Logs exception
+     * @param cause
+     */
+    fun exception(t: Exception) = CustomLog.logException(tag,t)
 
     /**
      * Logs message at info level
      * @param message labmda to be evaluated for message text
      */
-    inline operator fun invoke(message: () -> String) {
-        CustomLog.i(tag,message())
-    }
+    inline operator fun invoke(message: () -> String) = CustomLog.i(tag,message())
 
     /**
      * Logs message at info level
      * @param message labmda to be evaluated for message text
      */
-    inline fun info(message: () -> String) {
-        CustomLog.i(tag,message())
-    }
+    inline fun info(message: () -> String) = CustomLog.i(tag,message())
 
     /**
      * Logs message at info level
      * @param message labmda to be evaluated for message text
      */
-    inline fun i(message: () -> String) {
-        CustomLog.i(tag,message())
-    }
+    inline fun i(message: () -> String) = CustomLog.i(tag,message())
 
     /**
      * Logs message at log level
      * @param message labmda to be evaluated for message text
      */
-    inline fun l(message: () -> String) {
-        CustomLog.l(tag,message())
-    }
+    inline fun l(message: () -> String) = CustomLog.l(tag,message())
 
     /**
      * Logs message at log level
      * @param message labmda to be evaluated for message text
      */
-    inline fun log(message: () -> String) {
-        CustomLog.l(tag,message())
-    }
+    inline fun log(message: () -> String) = CustomLog.l(tag,message())
 
     /**
      * Logs message at debug level
      * @param message labmda to be evaluated for message text
      */
-    inline fun debug(message: () -> String) {
-        CustomLog.d(tag,message())
-    }
+    inline fun debug(message: () -> String) = CustomLog.d(tag,message())
+
 
     /**
      * Logs message at debug level
      * @param message labmda to be evaluated for message text
      */
-    inline fun d(message: () -> String) {
-        CustomLog.d(tag,message())
-    }
+    inline fun d(message: () -> String) = CustomLog.d(tag,message())
 
     /**
      * Logs message at verbose level
      * @param message labmda to be evaluated for message text
      */
-    inline fun v(message: () -> String) {
-        CustomLog.v(tag,message())
-    }
+    inline fun v(message: () -> String) = CustomLog.v(tag,message())
 
     /**
      * Logs message at verbose level
      * @param message labmda to be evaluated for message text
      */
-    inline fun verbose(message: () -> String) {
-        CustomLog.v(tag,message())
-    }
+    inline fun verbose(message: () -> String) = CustomLog.v(tag,message())
 
     /**
      * Logs message at warning level
      * @param message labmda to be evaluated for message text
      */
-    inline fun w(message: () -> String) {
-        CustomLog.w(tag,message())
-    }
+    inline fun w(message: () -> String) = CustomLog.w(tag,message())
 
     /**
      * Logs message at warning level
      * @param message labmda to be evaluated for message text
      */
-    inline fun warning(message: () -> String) {
-        CustomLog.w(tag,message())
-    }
+    inline fun warning(message: () -> String) = CustomLog.w(tag,message())
 
     /**
      * Logs message at error level
      * @param message labmda to be evaluated for message text
      */
-    inline fun e(message: () -> String) {
-        CustomLog.e(tag,message())
-    }
+    inline fun e(message: () -> String) = CustomLog.e(tag,message())
 
     /**
      * Logs message at error level
      * @param message labmda to be evaluated for message text
      */
-    inline fun error(message: () -> String) {
-        CustomLog.e(tag,message())
-    }
+    inline fun error(message: () -> String) = CustomLog.e(tag,message())
 
     /**
      * Put mark in log stream (see NSLogger's docs)
      * @param message labmda to be evaluated for mark text
      */
-    inline fun mark(message: () -> String) {
-        CustomLog.logMark(message())
-    }
+    inline fun mark(message: () -> String) = CustomLog.logMark(message())
+
 }
 
 /**
